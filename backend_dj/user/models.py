56746368f9c6f26f48108d77e776c69c147ecc65
KawshikBss <kawshikbss@gmail.com>
2022-08-26 11:01:16 +0600

    created users model and serializer

--- backend_dj/user/models.py
+++ backend_dj/user/models.py
@@ -1,3 +1,15 @@
+import email
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    username = models.CharField(max_length=200, null=False, blank=False)
+    email = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    profile_img = models.CharField(max_length=500, null=True, blank=True)
+    password = models.CharField(max_length=200, null=False, blank=False)
+    verified = models.BooleanField(default=False, null=True, blank=True)
+    admin = models.BooleanField(default=False, null=True, blank=True)
+    active = models.BooleanField(default=False, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
---
+++ backend_dj/user/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from rest_framework.serializers import Serializer
+from .models import User
+
+
+class UserSerializer(Serializer):
+    class Meta:
+        model = User
+        fields = '__all__'
