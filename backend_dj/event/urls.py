49940503c6ef047ba37d17188f66131cdec8f8f4
KawshikBss <kawshikbss@gmail.com>
2022-08-26 17:17:35 +0600

    included url routes for event operations

--- backend_dj/event/urls.py
+++ backend_dj/event/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('events-delete/<str:pk>', views.delete_event),
     path('events-user-all/<str:pk>', views.single_user_all_events),
     path('events-user-single/<str:event_pk>/<str:user_pk>', views.single_user_single_event),
+    path('events-user-completed/<str:pk>', views.single_user_completed_events),
+    path('events-user-missed/<str:pk>', views.single_user_missed_events),
+    path('events-user-active/<str:pk>', views.single_user_active_events),
 ]
