4216a31d8ed1085936ed755b6cc087aa04ff7c92
KawshikBss <kawshikbss@gmail.com>
2022-08-26 17:09:57 +0600

    included event app in settings and included event routes in url

--- backend_dj/backend_dj/urls.py
+++ backend_dj/backend_dj/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import urls as userUrls
+from event import urls as eventUrls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(userUrls)),
+    path('', include(eventUrls)),
 ]
