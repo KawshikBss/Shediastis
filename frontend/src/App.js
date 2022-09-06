78ad5fea1aa6b240ce08aea0ece90d5023d45ea9
KawshikBss <kawshikbss@gmail.com>
2022-09-07 03:33:06 +0600

    configured react query client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import Container from "./components/Container";
+import {
+  QueryClientProvider, QueryClient
+} from 'react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <Container />
-    </div>
+    <QueryClientProvider client={ queryClient }>
+      <div className="App">
+        <Container />
+      </div>
+    </QueryClientProvider>
   );
 }
 
